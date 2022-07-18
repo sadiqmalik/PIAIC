5df46675e9d8fb7edb3e505b12cae349cd125e6e
Sadiq Malik <subbahsadiqmalik@gmail.com>
2022-07-18 19:04:52 +0200

    Marksheet with function

---
+++ AIC/MarksheetWithFunction.py
@@ -0,0 +1,34 @@
+MAJOR_SUBJECT_MARKS = 100;
+MINOR_SUBJECT_MARKS = 50;
+TOTAL_MARKS = 400;
+
+def getMarks(subject, max_marks):
+    while True:
+        try:
+            numbers = int(input(subject + "'s marks: "))
+
+            if (numbers<=max_marks and numbers>0):
+                return numbers;
+
+            print("Invalid marks 0-100.")
+        except ValueError:
+            print("Only number allowed.")
+
+print("\n")
+
+physics = getMarks('Physic', MAJOR_SUBJECT_MARKS);
+chemistry = getMarks('Chemistry', MAJOR_SUBJECT_MARKS);
+maths = getMarks('Mathematics', MAJOR_SUBJECT_MARKS);
+english = getMarks('English', MINOR_SUBJECT_MARKS);
+arabic = getMarks('Arabic', MINOR_SUBJECT_MARKS);
+
+print("\n************* Student Marksheet *************\n")
+print("Physics\t\t\t  : " + str(physics))
+print("Chemistry\t\t  : " + str(chemistry))
+print("Mathematics\t\t  : " + str(maths))
+print("English\t\t\t  : " + str(english))
+print("Arabic\t\t\t  : " + str(arabic))
+
+obtained_marks = physics + chemistry + maths + english + arabic
+print("\n\nTotal Marks Obtained      :", obtained_marks, "out of", TOTAL_MARKS, sep=' ')
+print("Total Percentage Obtained :", round((obtained_marks*100)/TOTAL_MARKS, 2), "%", sep=' ', end='\n\n')
