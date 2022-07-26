276e88870da8bcd06a1f6b032e088d29c70ee956
Sadiq Malik <subbahsadiqmalik@gmail.com>
2022-07-26 10:09:06 +0200

    Print table improved code

--- AIC/Quarter1/Assignments/Lecture5_PrintTable.py
+++ AIC/Quarter1/Assignments/Lecture5_PrintTable.py
@@ -1,21 +1,18 @@
-random_list = []
 
-# Print table function
-def printTable():
-    while True:
-        try:
-            number = int(input("Enter number to print desired table: "))
-            for index in range(10):
-                print(number, "x", index+1, "=", (index+1)*number)
-
-            # Take input from user if they want to print more tables    
-            choice = str(input("\nDo you want to print more? y/n: "))
-            if (choice == 'y'):
-                continue;
-        
-            break;
-        except ValueError:
-            print("Only number allowed.")
-
+# Print table with desired numbers
 print("********************* Print Table *********************")
-printTable()
+
+while True:
+    try:
+        number = int(input("Enter number to print desired table: "))
+        for index in range(1,11):
+            print(f"{number} x {index} = {index*number}")
+
+        # Take input from user if they want to print more tables    
+        choice = str(input("\nDo you want to print more? y/n: "))
+        if (choice == 'y'):
+            continue;
+    
+        break;
+    except ValueError:
+        print("Only number allowed.")
