a3b6df2ca2af0327ea04c04ca5b68e6ebade6707
Sadiq Malik <subbahsadiqmalik@gmail.com>
2022-07-27 20:58:31 +0200

    Guess number program refined

--- AIC/Quarter1/Assignments/Lecture5_GuessRandomNumber.py
+++ AIC/Quarter1/Assignments/Lecture5_GuessRandomNumber.py
@@ -1,25 +1,22 @@
 import random
 
-# Guess the random number
-def guessRandomNumber():
-    # number of elements as input
-    random_number = random.randint(1, 50)
-
-    for i in range(1, 4):
-        while True:
-            try:
-                number = int(input("\nGuess the number 1-50: "))
-                break
-            except ValueError:
-                print("Only number allowed!")
-        
-        if (number == random_number):
-            print("Great! You guessed it correctly.")
-            break
-
-        print("Bad luck!")
-    
-    print("\nRandom number was: ", random_number)
-
 print("\n********************* Guess Random Number *********************")
-guessRandomNumber()
+
+# number of elements as input
+random_number = random.randint(1, 50)
+
+for i in range(1, 4):
+    while True:
+        try:
+            number = int(input("\nGuess the number 1-50: "))
+            break
+        except ValueError:
+            print("Only number allowed!")
+        
+    if (number == random_number):
+        print("Great! You guessed it correctly.")
+        break
+
+    print("Bad luck!")
+    
+print("\nRandom number was: ", random_number)
